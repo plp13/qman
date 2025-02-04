5045339eadc5b5c69a060dba7f928f1ca8e5a47d
Pantelis Panayiotou <info@miliaris.eu>
2025-02-04 20:17:40 +0200

    bump.sh now also supports versions with dashes and alphabetical characters

--- bump.sh
+++ bump.sh
@@ -17,19 +17,19 @@ else
   echo "Setting version to ${1}"
 
   echo "- meson.build"
-  sed -i "s/version: '[0-9][\.0-9]*'/version: '${1}'/g" meson.build
+  sed -i "s/version: '[a-zA-Z0-9][\.\a-zA-Z0-9]*'/version: '${1}'/g" meson.build
   exit_on_error $?
 
   echo "- src/config_def.py"
-  sed -i "s/\"program_version\": ((\"wstring\",), (\"Qman [0-9][\.0-9]*\",)/\"program_version\": ((\"wstring\",), (\"Qman ${1}\",)/g" src/config_def.py
+  sed -i "s/\"program_version\": ((\"wstring\",), (\"Qman [a-zA-Z0-9][\.\a-zA-Z0-9]*\",)/\"program_version\": ((\"wstring\",), (\"Qman ${1}\",)/g" src/config_def.py
   exit_on_error $?
 
   echo "- README.md"
-  sed -i "s/Version [0-9][\.0-9]* --/Version ${1} --/g" README.md
+  sed -i "s/Version [a-zA-Z0-9][\.\a-zA-Z0-9]* --/Version ${1} --/g" README.md
   exit_on_error $?
 
   echo "- man/qman.1.md"
-  sed -i "s/footer: Qman [0-9][\.0-9]*/footer: Qman ${1}/g" man/qman.1.md
+  sed -i "s/footer: Qman [a-zA-Z0-9][\.\a-zA-Z0-9]*/footer: Qman ${1}/g" man/qman.1.md
   exit_on_error $?
 
   cd man
