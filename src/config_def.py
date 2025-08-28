plp13/qman

commit f0c6babe39000d685e9be28a748d41a32519dfac

Replace extraneous calls to getenv("TERM") with tcap.term

man/qman.1
@@ -1,7 +1,7 @@
 '\" t
 .\" Automatically generated by Pandoc 3.1.12.3
 .\"
-.TH "QMAN" "1" "December 15, 2023" "Qman 1.5.0\-13\-g6dc98ee" "General Commands Manual"
+.TH "QMAN" "1" "December 15, 2023" "Qman 1.5.0\-14\-gf4bcae5" "General Commands Manual"
 .SH NAME
 \f[B]Qman\f[R] \- A more modern manual page viewer for our terminals
 .SH SYNOPSIS

meson.build
@@ -3,7 +3,7 @@
 # Project declaration
 project('qman', 'c',
   license: 'BSD-2-Clause',
-  version: '1.5.0-13-g6dc98ee'
+  version: '1.5.0-14-gf4bcae5'
 )
 
 # Executable

src/config_def.py
@@ -175,7 +175,7 @@ config_def = {
     },
     "misc": {
         "program_name": (("string",), None, False, "Program executable basename (discovered automatically)"),
-        "program_version": (("wstring",), ("Qman 1.5.0-13-g6dc98ee",), False, "Formal program name and version"),
+        "program_version": (("wstring",), ("Qman 1.5.0-14-gf4bcae5",), False, "Formal program name and version"),
         "system_type": (("systype", ), ("mandb", ), True, "System type: mandb, mandoc, freebsd, darwin, ..."),
         "config_path": (("string",), None, False, "Path to the configuration file"),
         "man_path": (("string",), ("/usr/bin/man",), True, "Path to the man(1) command"),

src/tui.c
@@ -631,8 +631,7 @@ void init_tui_mouse() {
               NULL);
 
     // Initialize terminal to enable drag-and-drop
-    char *term = getenv("TERM");
-    if (0 != strcmp(term, "xterm-1002")) {
+    if (0 != strcmp(tcap.term, "xterm-1002")) {
       sendescseq("[?1002h");
     }
   }
@@ -1375,8 +1374,7 @@ void winddown_tui() {
   reset_color_pairs();
 
   // Initialize terminal to disable drag-and-drop
-  char *term = getenv("TERM");
-  if (0 != strcmp(term, "xterm-1002")) {
+  if (0 != strcmp(tcap.term, "xterm-1002")) {
     sendescseq("[?1002l");
   }
 

commit 4d0fc6077658029cfaa0d4b1c8814b4bf4495758

Bugfix: crash on windown_tui() when in CLI mode, introduced with commit f0c6babe39000d685e9be28a748d41a32519dfac

man/qman.1
@@ -1,7 +1,7 @@
 '\" t
 .\" Automatically generated by Pandoc 3.1.12.3
 .\"
-.TH "QMAN" "1" "December 15, 2023" "Qman 1.5.0\-15\-gf0c6bab" "General Commands Manual"
+.TH "QMAN" "1" "December 15, 2023" "Qman 1.5.0\-16\-g28b9f7b" "General Commands Manual"
 .SH NAME
 \f[B]Qman\f[R] \- A more modern manual page viewer for our terminals
 .SH SYNOPSIS

meson.build
@@ -3,7 +3,7 @@
 # Project declaration
 project('qman', 'c',
   license: 'BSD-2-Clause',
-  version: '1.5.0-15-gf0c6bab'
+  version: '1.5.0-16-g28b9f7b'
 )
 
 # Executable

src/config_def.py
@@ -175,7 +175,7 @@ config_def = {
     },
     "misc": {
         "program_name": (("string",), None, False, "Program executable basename (discovered automatically)"),
-        "program_version": (("wstring",), ("Qman 1.5.0-15-gf0c6bab",), False, "Formal program name and version"),
+        "program_version": (("wstring",), ("Qman 1.5.0-16-g28b9f7b",), False, "Formal program name and version"),
         "system_type": (("systype", ), ("mandb", ), True, "System type: mandb, mandoc, freebsd, darwin, ..."),
         "config_path": (("string",), None, False, "Path to the configuration file"),
         "man_path": (("string",), ("/usr/bin/man",), True, "Path to the man(1) command"),

src/tui.c
@@ -1374,7 +1374,7 @@ void winddown_tui() {
   reset_color_pairs();
 
   // Initialize terminal to disable drag-and-drop
-  if (0 != strcmp(tcap.term, "xterm-1002")) {
+  if (NULL != tcap.term && 0 != strcmp(tcap.term, "xterm-1002")) {
     sendescseq("[?1002l");
   }
 